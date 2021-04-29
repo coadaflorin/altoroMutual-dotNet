b39c2e89531d12bfd050d0da438e5324e4d23984
The Florin <coadaflorin@gmail.com>
2021-04-29 16:58:22 +0100

    added some containers

---
+++ containers/NoDockerUserSpecifiedCLI.sh
@@ -0,0 +1,6 @@
+docker run e98b6ec72f51 -u 3000
+docker run e98b6ec72f51 -H tcp://0.0.0.0:2375
+docker container run --name container_instance e98b6ec72f51
+docker container run --name container_instance --user 4000 e98b6ec72f51 -H tcp://192.168.59.103:2375 
+docker run e98b6ec72f51 -h tcp://0.0.0.0:2375
+docker run fasdfasdf -u 0
