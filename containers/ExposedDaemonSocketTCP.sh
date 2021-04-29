71d84acee2005bf1828ab2a906ce5287b209d2f4
The Florin <coadaflorin@gmail.com>
2021-04-29 17:30:03 +0100

    added container config

---
+++ containers/ExposedDaemonSocketTCP.sh
@@ -0,0 +1,6 @@
+docker run e98b6ec72f51
+docker run e98b6ec72f51 -H tcp://0.0.0.0:2375
+docker container run --name container_instance e98b6ec72f51
+docker container run --name container_instance -H tcp://192.168.59.103:2375  e98b6ec72f51 
+docker run -H tcp://0.0.0.0:2375 e98b6ec72f51
+docker run e98b6ec72f51 -h tcp://0.0.0.0:2375
